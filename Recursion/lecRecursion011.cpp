7e9af303cfa613829b58609219eab9eaa402b8e9
Ayushman <maanayush1002@gmail.com>
2023-01-09 22:04:43 +0530

    Binary Search
    C++ program to implement binary Search

---
+++ Recursion/lecRecursion011.cpp
@@ -0,0 +1,61 @@
+// c++ program to implement binary Search
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int binarySearch(int *arr, int start, int end, int key)
+{
+    int mid = (start + end) / 2;
+    if (arr[mid] == key)
+    {
+        return mid;
+    }
+
+    else if (key > arr[mid])
+    {
+        return binarySearch(arr, mid + 1, end, key);
+    }
+    else if (key < arr[mid])
+    {
+        return binarySearch(arr, start, mid - 1, key);
+    }
+    else
+    {
+        return -1;
+    }
+}
+int main()
+{
+    int key;
+
+    int *arr;
+    arr = (int *)malloc(sizeof(int));
+
+    int size;
+    cout << "Enter the size of the array : \t";
+    cin >> size;
+
+    cout << "Enter the elements of the array :" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        /* code */
+        cin >> arr[i];
+    }
+
+    cout << "Enter the key you are searching for : \t";
+    cin >> key;
+
+    int check = binarySearch(arr, 0, size, key);
+
+    if (check == -1)
+    {
+        cout << "Key NOT present in the array" << endl;
+    }
+    else
+    {
+        cout << "Key found in the array at index : \t" << check << endl;
+    }
+
+    return 0;
+}
