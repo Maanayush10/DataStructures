c7ae7289033a65f33bc2a2ffa363d0c2ba1f4cdb
Ayushman <maanayush1002@gmail.com>
2023-01-13 23:14:13 +0530

    Power
    C++ program to calculate a raised to power b

---
+++ Recursion/lecRecursion014.cpp
@@ -0,0 +1,32 @@
+//C++ program to calculate a raised to power b
+
+#include<iostream>
+using namespace std;
+int count=0;
+int calcPower(int a, int b)
+{
+    // count++;
+    if(b==1)
+    {
+        return a;
+    }
+    else
+    {
+        return a*calcPower(a, b-1);
+    }
+
+}
+int main(){
+    int a , b;
+    cout<<"Enter the value of a :\t";
+    cin>>a;
+    cout<<"Enter the value of b :\t";
+    cin>>b;
+
+    int power= calcPower(a ,b);
+
+    cout<<"The expected output is :" <<power<<endl;
+
+
+return 0;
+}
