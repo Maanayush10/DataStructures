33f54418f7593fd70ef6a050aa86a3a69efe27a1
Ayushman <maanayush1002@gmail.com>
2023-03-27 15:22:47 +0530

    Update fibonacci.cpp

--- DP/fibonacci.cpp
+++ DP/fibonacci.cpp
@@ -2,6 +2,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//bottom-up approach
+int fib2(int n)
+{
+    int dp[n+1];
+
+    dp[0]=0;
+    dp[1]=1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i]= dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+//top down apprach
 int fib(int n, vector<int> &dp)
 {
     if (n == 0 || n == 1)
@@ -27,7 +42,8 @@ int main()
         dp[i] = -1;
     }
     int fibNum = fib(num, dp);
-    cout << "The nth fibonacci number is :\t" << fibNum;
+    cout << "The nth fibonacci number is :\t" << fibNum<<endl;
+    cout << "The nth fibonacci number is :\t" << fib2(num)<<endl;
 
     return 0;
 }
