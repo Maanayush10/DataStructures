005cb3276757d07925c17224bb9726abc450b0ec
Ayushman <maanayush1002@gmail.com>
2023-03-29 23:56:23 +0530

    Update minCostDp.cpp

--- DP/minCostDp.cpp
+++ DP/minCostDp.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//top-down approach DP
 int solve(vector<int> &cost, int n, vector<int>&dp)
 {
     if(n==0 || n==1)
@@ -17,13 +18,28 @@ int solve(vector<int> &cost, int n, vector<int>&dp)
     return dp[n];
 }
 
+//Bottom up approach DP tabulation
+int solve2(vector<int> &cost, int n)
+{
+    vector<int> dp(n+1);
+    dp[0]=cost[0];
+    dp[1]=cost[1];
+    for(int i=2; i<=n; i++)
+    {
+        dp[i]= min(dp[i-1],dp[i-2])+cost[i];
+    }
+    return dp[n];
+}
+
 int main()
 {
-    vector<int> cost={10,15,20};
+    vector<int> cost={1,100,1,1,1,100,1,1,100,1};
     int n= cost.size();
     vector<int> dp(n+1, -1);
 
     int ans = min(solve(cost, n-1 ,dp),solve(cost, n-2, dp));
+    int ans2 = min(solve2(cost, n-1),solve2(cost, n-2));
+    cout<<"Minimum cost: "<<ans<<endl;
     cout<<"Minimum cost: "<<ans<<endl;
     
 return 0;
