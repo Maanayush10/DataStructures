37e27b247a2b8766d71d3964d145e82ed0596aff
Ayushman <maanayush1002@gmail.com>
2023-03-30 11:24:18 +0530

    Update minCostDp.cpp

--- DP/minCostDp.cpp
+++ DP/minCostDp.cpp
@@ -31,6 +31,23 @@ int solve2(vector<int> &cost, int n)
     return dp[n];
 }
 
+
+//space optimisation
+int solve3(vector<int> &cost, int n)
+{
+    // vector<int> dp(n+1);
+    int prev2=cost[0];
+    int prev1=cost[1];
+    for(int i=2; i<n; i++)
+    {
+        int curr= min(prev1, prev2)+cost[i];
+        prev2= prev1;
+        prev1 = curr;
+
+    }
+    return min(prev1, prev2);
+}
+
 int main()
 {
     vector<int> cost={1,100,1,1,1,100,1,1,100,1};
@@ -39,8 +56,10 @@ int main()
 
     int ans = min(solve(cost, n-1 ,dp),solve(cost, n-2, dp));
     int ans2 = min(solve2(cost, n-1),solve2(cost, n-2));
+    int ans3 = solve3(cost, n);
     cout<<"Minimum cost: "<<ans<<endl;
-    cout<<"Minimum cost: "<<ans<<endl;
+    cout<<"Minimum cost: "<<ans2<<endl;
+    cout<<"Minimum cost: "<<ans3<<endl;
     
 return 0;
 }
