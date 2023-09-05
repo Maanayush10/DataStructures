d306d302006c10efde578ade8f407ece46400001
Ayushman <maanayush1002@gmail.com>
2023-09-05 23:26:35 +0530

    added top-down approach

--- DP/0-1KnapSack.cpp
+++ DP/0-1KnapSack.cpp
@@ -1,4 +1,6 @@
 //GFG 0-1 knapsack solution (Aditya Verma Tutorial)
+
+//recursive + memoization
 class Solution
 {
     public:
@@ -33,3 +35,38 @@ class Solution
        }
     }
 };
+
+
+//top down approach
+ int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       //base case
+       int t[n+1][W+1];
+       for(int i=0; i<n+1; i++)
+       {
+           for(int j=0; j<W+1; j++)
+           {
+               if(i==0 || j==0)
+               {
+                   t[i][j]=0;
+               }
+           }
+       }
+       //Recursive cases
+      for(int i=1; i<n+1; i++)
+      {
+          for(int j=1; j<W+1; j++)
+          {
+              if(wt[i-1]<=j)
+              {
+                  t[i][j]= max( val[i-1]+t[i-1][j-wt[i-1]], t[i-1][j]);
+              }
+              else
+              {
+                  t[i][j]= t[i-1][j];
+              }
+          }
+      }
+      return t[n][W];
+    }
