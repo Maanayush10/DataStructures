b96ebd88849295cee0ee82d46a5ee21f8972c668
Ayushman <maanayush1002@gmail.com>
2023-09-27 09:26:14 +0530

    Create upperBound.cpp

---
+++ Binary-Search-Problems/upperBound.cpp
@@ -0,0 +1,29 @@
+//implementing upper bound problem
+#include<bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n){
+	// Write your code here.	
+	int low= 0, high= n-1;
+	int ans=n;
+	while(low<=high)
+	{
+		int mid= low+(high-low)/2;
+		if(arr[mid]>x)
+		{
+			ans= mid;
+			high= mid-1;
+		}
+		else
+		{
+			low= mid+1;
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+  //main logic here
+  return 0;
+}
