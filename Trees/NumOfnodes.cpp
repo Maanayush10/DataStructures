f744ab30fb35711abfa82cb2f5fbb7813d287c68
Ayushman <maanayush1002@gmail.com>
2023-10-20 22:43:34 +0530

    Create NumOfnodes.cpp

---
+++ Trees/NumOfnodes.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int numberOfNodes(int N){
+    // Write your code here.
+    return pow(2, N-1);
+}
+
+int main()
+{
+  //main logic here
+  return 0;
+}
