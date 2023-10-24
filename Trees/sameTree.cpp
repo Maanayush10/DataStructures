86bc090949921412520ab8860ca81aacfa98511e
Ayushman <maanayush1002@gmail.com>
2023-10-24 23:36:36 +0530

    Create sameTree.cpp

---
+++ Trees/sameTree.cpp
@@ -0,0 +1,19 @@
+//shoutout to striver
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL || q==NULL)
+        {
+            return p==q;
+        }
+        return p->val==q->val && isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
+    }
+
+
+int main()
+{
+  //Main logic here;
+  return 0;
+}
