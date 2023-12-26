b15f2fe231546b54b69446bce45b30e287b769b2
Ayushman <maanayush1002@gmail.com>
2023-12-26 20:39:52 +0530

    Update NinjaTraining.cpp

--- DP/NinjaTraining.cpp
+++ DP/NinjaTraining.cpp
@@ -2,6 +2,39 @@
 using namespace std;
 
 //shouttout to striver
+//memoization solution
+int f(int day, int last, vector<vector<int>> &points, vector<vector<int>>&dp)
+{
+    int result=0;
+    if(dp[day][last]!=-1){return dp[day][last];}
+    if(day==0)
+    {
+        int maxi=INT_MIN;
+        for(int i=0; i<3; i++)
+        {
+            if(i!=last)
+            {
+                maxi= max(maxi, points[0][i]);
+            }
+        }
+        return dp[day][last]=maxi;
+    }
+
+    int maxi=INT_MIN;
+    for(int j=0; j<3; j++)
+    {
+        if(j!=last)
+        {
+            int currPoints=points[day][j]+f(day-1, j, points,dp);
+            maxi= max(maxi,currPoints);
+        }
+    }
+    dp[day][last]= maxi;
+    return dp[day][last];
+}
+
+
+
 //recursive solution
 int f(int day, int last, vector<vector<int>> &points)
 {
